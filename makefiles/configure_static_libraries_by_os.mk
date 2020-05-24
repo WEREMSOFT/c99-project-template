c188e8f6374969a7045d2afd45757d028ef9ef86
weremsoft <pablo.weremczuk@gmail.com>
2020-05-24 12:25:23 +0200

    [DEL] make folder

--- makefiles/configure_static_libraries_by_os.mk
+++
@@ -1,8 +0,0 @@
-# Set static libraries depending on de dettected OS
-ifeq ($(DETTECTED_OS),Linux)
-	LINK_LIBS := 
-	#LINK_LIBS := -l:libraylib-linux.a -l:libglfw3.a -lm -ldl -lpthread -lX11 -lxcb -lGL -lGLX -lXext -lGLdispatch -lXau -lXdmcp
-else ifeq ($(DETTECTED_OS),Darwin)
-	LINK_LIBS := 
-	#LINK_LIBS := -O0 -framework IOKit -v -lraylib -framework OpenGL -framework cocoa 
-endif
--- makefiles/dettect_os.mk
+++
@@ -1,7 +0,0 @@
-# check which OS Am I
-ifeq ($(OS),Windows_NT)
-	DETTECTED_OS := $(OS)
-else
-	UNAME_S := $(shell uname -s)
-	DETTECTED_OS := $(UNAME_S)
-endif
